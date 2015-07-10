1698df5ac1a64d80d253a9105769ec647b6f1da3
Nathan Merritt <gnm@the-merritts.net>
2015-07-09 21:02:57 -0400

    added actual settings

--- cron.sh
+++ cron.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOG="./logs/cron.log"
+HOSTNAME=`hostname`
+export CASINO_SETTINGS=~/deployments/config/casino/${HOSTNAME}.py
 cd $DIR
 source ~/venvs/casino/bin/activate
 
