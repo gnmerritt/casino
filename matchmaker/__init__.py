37088f83e94ad7fb61be46150ffb1dffde482829
Nathan Merritt <gnm@the-merritts.net>
2015-08-11 18:18:32 -0400

    added robots.txt

--- matchmaker/__init__.py
+++ matchmaker/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config.from_object('matchmaker.default_settings')
 app.config.from_envvar('CASINO_SETTINGS', silent=True)
 
--- matchmaker/views.py
+++ matchmaker/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory, request
 from flask.ext.login import login_required, current_user
 
 from matchmaker import app
@@ -8,6 +8,10 @@ from profile import PlayerData
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/robots.txt')
+def static_from_route():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/profile')
 @login_required
