140bd412ab042b32d9e5a515adf99e9f9b9cd846
Nathan Merritt <gnm@the-merritts.net>
2015-08-12 22:29:15 -0400

    switched to postgres, long live postgres

--- matchmaker/default_settings.py
+++ matchmaker/default_settings.py
@@ -1,5 +1,5 @@
 DEBUG = True
 SECRET_KEY = "development"
-SQLALCHEMY_DATABASE_URI = "sqlite:///matchmaker.db"
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://poker:1234@localhost:5432/poker"
 CASINO_HOST = "localhost"
 CASINO_PORT = 5154
