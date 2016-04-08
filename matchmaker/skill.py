eace27e618187860564de9501aabcb360112025a
Nathan Merritt <gnm@the-merritts.net>
2016-04-07 21:57:45 -0400

    Add bots with no games to the leaderboard

--- matchmaker/skill.py
+++ matchmaker/skill.py
@@ -1,6 +1,6 @@
 import datetime
 from sqlalchemy.sql import func
-from models import MatchResult, BotSkill, BotRank
+from models import MatchResult, BotSkill, BotRank, BotIdentity
 
 
 class SkillUpdater(object):
@@ -8,21 +8,30 @@ class SkillUpdater(object):
         session = db.session
         today = datetime.date.today()
         skills = session.query(
-            MatchResult.bot,
-            func.sum(MatchResult.delta_chips),
-            func.sum(MatchResult.hands)
+            BotIdentity.id,
+            func.coalesce(func.sum(MatchResult.delta_chips), 0),
+            func.coalesce(func.sum(MatchResult.hands), 0),
             ) \
-            .group_by(MatchResult.bot) \
+            .outerjoin(MatchResult) \
+            .group_by(BotIdentity.id) \
             .all()
         yesterday = today - datetime.timedelta(days=1)
         yesterdays_skills = {b.bot: b.skill for b in
                              BotSkill.query.filter_by(date=yesterday).all()}
         BotSkill.query.filter_by(date=today).delete()
         session.bulk_save_objects(
-            [BotSkill(s[0], today, s[1] / s[2], yesterdays_skills.get(s[0], 0))
+            [BotSkill(s[0], today,
+                      self.calc_winnings_per_hand(s[1], s[2]),
+                      yesterdays_skills.get(s[0], 0))
              for s in skills]
         )
         session.commit()
+
+    def calc_winnings_per_hand(self, chips, hand):
+        try:
+            return chips / hand
+        except ZeroDivisionError:
+            return 0
 
 
 class RankUpdater(object):
